f44d9625d1eadd35256078f11dedeb10921b0574
karateka <karateka004.2@gmail.com>
2026-06-01 21:12:09 +0200

    Updates

--- assets/react-CFuisBQD.js
+++ assets/react-CFuisBQD.js
@@ -1,4 +1,4 @@
-import{a7 as _a}from"./vendor-phNjiF6F.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{a8 as _a}from"./vendor-B_zJODOX.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
