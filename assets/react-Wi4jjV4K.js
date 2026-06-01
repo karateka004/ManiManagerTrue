d18584557502c0d969a1ec62d039e60a7900c67f
karateka <karateka004.2@gmail.com>
2026-06-01 08:05:10 +0200

    Updates

--- assets/react-Wi4jjV4K.js
+++ assets/react-Wi4jjV4K.js
@@ -1,4 +1,4 @@
-import{a7 as _a}from"./vendor-B7Xuz7Qz.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{a7 as _a}from"./vendor-phNjiF6F.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
