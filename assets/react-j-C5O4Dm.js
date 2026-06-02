7ee72df82c5adccb1a6d0f3474cebb76213d8076
karateka <karateka004.2@gmail.com>
2026-06-02 07:15:13 +0200

    Updates

--- assets/react-j-C5O4Dm.js
+++ assets/react-j-C5O4Dm.js
@@ -1,4 +1,4 @@
-import{a8 as _a}from"./vendor-B_zJODOX.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{ac as _a}from"./vendor-BYCnHbLo.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
