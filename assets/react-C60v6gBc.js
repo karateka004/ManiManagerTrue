ca03bd7c487535065c373068ba19cff4ccd1fe31
karateka <karateka004.2@gmail.com>
2026-06-16 13:12:39 +0200

    Updates

--- assets/react-C60v6gBc.js
+++ assets/react-C60v6gBc.js
@@ -1,4 +1,4 @@
-import{ac as _a}from"./vendor-NoSe56Qi.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{ab as _a}from"./vendor-g5fqB22b.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
