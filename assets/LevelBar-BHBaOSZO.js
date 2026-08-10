fe89467ff02f69d88c61d12169481e174402c1ce
karateka004 <karateka004@gmail.com>
2026-08-10 15:27:19 +0200

    Updates

---
+++ assets/LevelBar-BHBaOSZO.js
@@ -0,0 +1,1 @@
+import{aR as t,ac as n,x as r}from"./index-Dsg-VOqe.js";import"./vendor-Db2M4VL8.js";function c(){const s=t(o=>o.transactions.length),e=t(o=>o.bonusXp);return n(r(s,e))}export{c as u};
--- assets/LevelBar-DBsA_5Di.js
+++
@@ -1,1 +0,0 @@
-import{aQ as t,ac as n,x as r}from"./index-D6QW41kO.js";import"./vendor-Db2M4VL8.js";function c(){const s=t(o=>o.transactions.length),e=t(o=>o.bonusXp);return n(r(s,e))}export{c as u};
