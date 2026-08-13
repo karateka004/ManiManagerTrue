f90f2ec04db1c97615ccb9a5ebd7f9f78a0ed859
karateka004 <karateka004@gmail.com>
2026-08-13 22:17:43 +0200

    Updates

--- assets/LevelBar-fGI3PpGZ.js
+++
@@ -1,1 +0,0 @@
-import{b0 as t,aj as n,z as r}from"./index-i_YQvb6e.js";import"./vendor-0j5WbGRA.js";function p(){const s=t(o=>o.transactions.length),e=t(o=>o.bonusXp);return n(r(s,e))}export{p as u};
